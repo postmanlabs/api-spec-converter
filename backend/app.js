2da7dafd5398798e357d243c5ff15c1e534ef6cd
Sarthak Saxena <saxenasarthak72@gmail.com>
2021-06-08 18:26:12 +0530

    init setup

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const converterRouter = require('./src/routers/converterRouter')
+const configRouter = require('./src/routers/configRouter')
+const path = require('path')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(converterRouter)
+app.use(configRouter)
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+module.exports = app

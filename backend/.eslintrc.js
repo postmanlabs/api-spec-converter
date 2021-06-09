e41af36dae1d95956bbb4d71d29c1b51f3f1d5ed
Sarthak Saxena <saxenasarthak72@gmail.com>
2021-06-09 23:09:34 +0530

    Backend: express boilerplate and node env in eslint config

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 12,
+        'sourceType': 'module'
     },
-    "rules": {
-        "semi": ["error", "never"],
-        "quotes": ["error", "single"]
+    'rules': {
+        'semi': ['error', 'never'],
+        'quotes': ['error', 'single']
     }
-};
+}

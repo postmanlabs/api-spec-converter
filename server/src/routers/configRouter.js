9b500bc9dc044e5b3c7586ae6372de16a1e14fa2
Sarthak Saxena <saxenasarthak72@gmail.com>
2021-06-12 17:48:17 +0530

    #10 | New dependency - Lucy's converter npm package  api-spec-converter, initialized the config and the configRouter which will be used to fetch available conversions

---
+++ server/src/routers/configRouter.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = new express.Router()
+
+const { mapping } = require('../constants/config')
+
+router.use(express.json())
+
+router.get('/mapping', async (req, res) => {
+  return res.status(200).send(mapping);
+})
+
+
+module.exports = router

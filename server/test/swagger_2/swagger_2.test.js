9fe2ef1bccc7e2408806fd1b773ea9ede127f1db
sarthak_saxena_72 <saxenasarthak72@gmail.com>
2021-07-03 16:09:54 +0530

    #10 | test: Add tests for all possible oas2 -> oas3 conversions, between json and yaml.

--- server/test/swagger_2/swagger_2.test.js
+++ server/test/swagger_2/swagger_2.test.js
@@ -4,12 +4,43 @@ const request = supertest(app)
 
 jest.setTimeout(30000)
 
-//Swagger 2 to OpenAPI 3
 describe('POST /api/specification/:format/:convertTo', () => {
-  it('swagger_2 to openapi_3', async () => {
+  //Swagger 2 (JSON) to OpenAPI 3 (JSON)
+  it('should convert swagger_2 (json) to openapi_3 (json) ', async () => {
+    const res = await request
+      .post('/api/specification/swagger_2/openapi_3')
+      .attach('file', 'test/swagger_2/data/swagger_2.json')
+    expect(res.status).toBe(200)
+    expect(typeof res.body.spec).toBe('object')
+  })
+
+  //Swagger 2 (JSON) to OpenAPI 3 (YAML)
+  it('should convert swagger_2 (json) to openapi_3 (yaml) ', async () => {
+    const res = await request
+      .post('/api/specification/swagger_2/openapi_3')
+      .attach('file', 'test/swagger_2/data/swagger_2.json')
+      .query({'syntax': 'yaml'})
+    expect(res.status).toBe(200)
+    expect(typeof res.body.spec).toBe('string')
+  })
+
+  //Swagger 2 (YAML) to OpenAPI 3 (JSON)
+  it('should convert swagger_2 (yaml) to openapi_3 (json) ', async () => {
     const res = await request
       .post('/api/specification/swagger_2/openapi_3')
       .attach('file', 'test/swagger_2/data/swagger_2.yaml')
     expect(res.status).toBe(200)
+    expect(typeof res.body.spec).toBe('object')
+  })
+
+  //Swagger 2 (YAML) to OpenAPI 3 (YAML)
+  it('should convert swagger_2 (json) to openapi_3 (json) ', async () => {
+    const res = await request
+      .post('/api/specification/swagger_2/openapi_3')
+      .attach('file', 'test/swagger_2/data/swagger_2.yaml')
+      .query({'syntax': 'yaml'})
+    expect(res.status).toBe(200)
+    console.log(res)
+    expect(typeof res.body.spec).toBe('string')
   })
 })

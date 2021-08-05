b435b58afd535e351d41ca83b1c3d87d41e7848c
sarthak_saxena_72 <saxenasarthak72@gmail.com>
2021-08-05 19:27:18 +0530

    #27 | test: Add tests for string input.

--- server/test/converterRouter/swagger_2/swagger_2.test.js
+++ server/test/converterRouter/swagger_2/swagger_2.test.js
@@ -1,9 +1,12 @@
+const fs = require('fs');
 const supertest = require('supertest');
 
 const app = require('../../../app');
 const request = supertest(app);
 
-const jsonResponse1 = require('./data/output/jsonResponse1.json'),
+const swagger_2_json = require('./data/input/swagger_2.json'),
+  swagger_2_yaml = fs.readFileSync('test/converterRouter/swagger_2/data/input/swagger_2.yaml').toString(),
+  jsonResponse1 = require('./data/output/jsonResponse1.json'),
   jsonResponse2 = require('./data/output/jsonResponse2.json'),
   { yamlResponse1, yamlResponse2 } = require('./data/output/yamlResponses');
 
@@ -118,5 +121,57 @@ describe('POST /api/specification/swagger_2/openapi_3', () => {
         expect(res.body.toString()).toEqual(yamlResponse1);
         done();
       });
+  });
+
+  //Swagger 2 (JSON) String Input to OpenAPI 3 (JSON) - Download JSON Response
+  it('should convert swagger_2 (json) string input to openapi_3 (json) and download json response', async () => {
+    const res = await request
+      .post('/api/specification/swagger_2/openapi_3')
+      .set({
+        'Content-Type': 'text/html'
+      })
+      .send(JSON.stringify(swagger_2_json))
+      .query({'toFile': true})
+      .buffer()
+      .parse((res, callback) => {
+        res.setEncoding('binary');
+        res.data = '';
+        res.on('data', (chunk) => {
+          res.data += chunk;
+        });
+        res.on('end', () => {
+          callback(null, Buffer.from(res.data, 'binary'));
+        });
+      });
+    expect(res.status).toBe(200);
+    expect(res.headers['content-type']).toEqual('application/json; charset=UTF-8');
+    expect(res.headers['content-disposition']).toEqual('attachment; filename="swagger_2-to-openapi_3.json"');
+    expect(JSON.parse(res.body)).toEqual(jsonResponse1);
+  });
+
+  //Swagger 2 (YAML) String Input to OpenAPI 3 (JSON) - Download JSON Response
+  it('should convert swagger_2 (yaml) string input to openapi_3 (json) and download json response', async () => {
+    const res = await request
+      .post('/api/specification/swagger_2/openapi_3')
+      .set({
+        'Content-Type': 'text/html'
+      })
+      .send(swagger_2_yaml)
+      .query({'toFile': true})
+      .buffer()
+      .parse((res, callback) => {
+        res.setEncoding('binary');
+        res.data = '';
+        res.on('data', (chunk) => {
+          res.data += chunk;
+        });
+        res.on('end', () => {
+          callback(null, Buffer.from(res.data, 'binary'));
+        });
+      });
+    expect(res.status).toBe(200);
+    expect(res.headers['content-type']).toEqual('application/json; charset=UTF-8');
+    expect(res.headers['content-disposition']).toEqual('attachment; filename="swagger_2-to-openapi_3.json"');
+    expect(JSON.parse(res.body)).toEqual(jsonResponse2);
   });
 });
